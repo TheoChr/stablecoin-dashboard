e79f2faf87e0e5d4949577499971fd0f83fdef2d
TheoChr <32749677+TheoChr@users.noreply.github.com>
2025-03-11 21:19:30 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StablecoinDashboard from './components/StablecoinDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <StablecoinDashboard />
+    </div>
+  );
+}
+
+export default App;
